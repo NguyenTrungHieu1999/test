3db8522bf87de288b8ac425ac7cfb5419a242ce5
Hieu Nguyen <nguyentrunghieu25051999@gmail.com>
2021-07-19 09:11:59 +0700

    .

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import MessengerCustomerChat from 'react-messenger-customer-chat';
 function App() {
   return (
     <div className="App">
-       <MessengerCustomerChat
-          pageId="100225912358943"
-          appId="136478068166816"
-        />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+ 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <MessengerCustomerChat
+          pageId="100225912358943"
+          appId="136478068166816"
+        />,
+  document.getElementById('demo')
 );
 
 // If you want to start measuring performance in your app, pass a function
