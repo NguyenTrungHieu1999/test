eee2457d697ad3fc9088972d06cc19c801484117
Hieu Nguyen <nguyentrunghieu25051999@gmail.com>
2021-07-19 09:06:19 +0700

    .

--- src/App.js
+++ src/App.js
@@ -5,21 +5,7 @@ import MessengerCustomerChat from 'react-messenger-customer-chat';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <MessengerCustomerChat
+       <MessengerCustomerChat
           pageId="100225912358943"
           appId="136478068166816"
         />
