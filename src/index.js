a690d92e89f788748f7e32b769a2dadd31eec873
Hieu Nguyen <nguyentrunghieu25051999@gmail.com>
2021-07-19 09:21:06 +0700

    .

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
+  
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MessengerCustomerChat from 'react-messenger-customer-chat';
- 
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
 ReactDOM.render(
-  <MessengerCustomerChat
-    pageId="136478068166816"
-    appId="100225912358943"
-    htmlRef="<REF_STRING>"
-  />,
-  document.getElementById('demo')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
