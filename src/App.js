ee992c5a0733ae8cf0e3d1a06be03c6c3ba0181f
Hieu Nguyen <nguyentrunghieu25051999@gmail.com>
2021-07-19 08:56:33 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+    const App = () => (  
+      <div>
+        <MessengerCustomerChat
+          pageId="100225912358943"
+          appId="136478068166816"
+        />
+      </div>
+    );
 
 export default App;
